0c2334826bff4339dc413ee901beb18c24499fc4
plud299 <19520223@gm.uit.edu.vn>
2021-11-02 10:30:55 +0700

    Update ...

--- Meeting Chat/assets/css/popup.js
+++ Meeting Chat/assets/css/popup.js
@@ -1,14 +1,15 @@
-const btn_login = querySelector(".header__navbar-item .header__navbar-item--strong .header__navbar-item--separate")
-const btn_register = querySelector(".header__navbar-item .header__navbar-item--strong")
-const btn_register_form = querySelector(".auth-form__switch_btn")
-const btn_login_form = querySelector(".auth-form__switch_btn")
-const modal = querySelector(".modal")
-const register_form = querySelector("#register_form")
-const login_form = querySelector("#login_form")
-
+const btn_login = document.querySelector("#btn_login")
+const btn_register = document.querySelector("#btn_register")
+const btn_register_form = document.querySelector(".auth-form__switch_btn")
+const btn_login_form = document.querySelector(".auth-form__switch_btn")
+const modal = document.querySelector("#modal")
+const register_form = document.querySelector("#register_form")
+const login_form = document.querySelector("#login_form")
+console.log(btn_login)
 btn_login.onClick = function(){
-    modal.style.display = block
-    login_form.style.display = block
-    
+    modal.style.display = 'block'
+    login_form.style.display = 'block'
+    register_form.style.display = 'none'
+
 
 }
