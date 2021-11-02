f6f46cdb35a7dbfca2920d561cbe1446c71751af
plud299 <19520223@gm.uit.edu.vn>
2021-11-02 11:30:37 +0700

    Update js

--- Meeting Chat/assets/css/popup.js
+++ Meeting Chat/assets/css/popup.js
@@ -1,15 +1,42 @@
-const btn_login = document.querySelector("#btn_login")
-const btn_register = document.querySelector("#btn_register")
-const btn_register_form = document.querySelector(".auth-form__switch_btn")
-const btn_login_form = document.querySelector(".auth-form__switch_btn")
-const modal = document.querySelector("#modal")
-const register_form = document.querySelector("#register_form")
-const login_form = document.querySelector("#login_form")
-console.log(btn_login)
-btn_login.onClick = function(){
-    modal.style.display = 'block'
-    login_form.style.display = 'block'
-    register_form.style.display = 'none'
+const btn_register = document.querySelector("#btn_register");
+const btn_login = document.querySelector("#btn_login");
+const modal = document.querySelector("#modal");
+const register_form = document.querySelector("#register-form");
+const login_form = document.querySelector("#login-form");
+const btn_login_form = document.querySelector("#btn_login_form");
+const btn_register_form = document.querySelector("#btn_register_form");
+const btn_back = document.querySelectorAll(
+  ".btn.btn--normal.auth-form__controls-back"
+);
 
+btn_register.onclick = function () {
+  modal.style.display = "block";
+  register_form.style.display = "block";
+  login_form.style.display = "none";
+  btn_back[0].onclick = function () {
+    modal.style.display = "none";
+    register_form.style.display = "none";
+    login_form.style.display = "none";
+  };
+};
 
-}
+btn_login.onclick = function () {
+  modal.style.display = "block";
+  register_form.style.display = "none";
+  login_form.style.display = "block";
+  btn_back[1].onclick = function () {
+    modal.style.display = "none";
+    register_form.style.display = "none";
+    login_form.style.display = "none";
+  };
+};
+
+btn_login_form.onclick = function () {
+  register_form.style.display = "none";
+  login_form.style.display = "block";
+};
+
+btn_register_form.onclick = function () {
+  register_form.style.display = "block";
+  login_form.style.display = "none";
+};
